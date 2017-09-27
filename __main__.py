df8506f46b051e901a0b063967d439582a94a1d6
Aditya Kumar <kumar.sava.aditya@gmail.com>
2017-09-27 23:51:51 +0530

    add functionality to download the first search result

--- __main__.py
+++ __main__.py
@@ -1,11 +1,11 @@
 import requests
+from os import system
 from bs4 import BeautifulSoup
+from youtube_dl import YoutubeDL
 
 base_url = "https://youtube.com/results?search_query="
-current_page = 0
 
-
-search_key = '+'.join(input("Enter song name: ").split(' '))
+search_key = '+'.join(input("Enter search term: ").split(' '))
 url = base_url + search_key
 
 search_request = requests.get(url)
@@ -15,7 +15,19 @@ print(search_request.status_code) #200
 
 search_soup = BeautifulSoup(search_request.text, 'html.parser')
 
-links = search_soup.findAll('a', {'class': 'spf-link'})
+vid_links = []
+links = search_soup.findAll('a', {'class': 'yt-uix-tile-link'})
 for link in links:
-    if 'watch' in link.get('href'):
-        print('https://www.youtube.com/'+link.get('href'))
+    href = link.get('href')
+    #print(href)
+    if 'watch' in href and 'list' not in href:
+        vid_links.append([link.text, 'https://www.youtube.com/'+href])
+
+for link in vid_links:
+    print(link[0])
+    print('\t',link[1])
+
+print('\n\nDownloading:', vid_links[0][1])
+command = "youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 '{url}'".format(url=vid_links[0][1])
+system(command)
+
